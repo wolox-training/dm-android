9187771a7091eae3b79701672bffad83c1717c0d
Dylan Muszel <dylan.muszel@wolox.com.ar>
2018-08-08 10:26:20 -0300

    First commit

---
+++ app/src/main/java/ar/com/wolox/android/example/BaseConfiguration.java
@@ -0,0 +1,8 @@
+package ar.com.wolox.android.example;
+
+class BaseConfiguration {
+
+    public static final String EXAMPLE_CONFIGURAITON_KEY = "https://jsonplaceholder.typicode.com";
+    public static final String SHARED_PREFERENCES_NAME = "private-shared-prefs";
+
+}
