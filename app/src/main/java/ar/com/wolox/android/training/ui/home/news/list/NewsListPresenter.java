b9c75706e771964291ee084da4cdb89381d8acad
Dylan Muszel <dylan.muszel@wolox.com.ar>
2018-08-15 14:21:21 -0300

    Removing tests

--- app/src/main/java/ar/com/wolox/android/training/ui/home/news/list/NewsListPresenter.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/home/news/list/NewsListPresenter.java
@@ -42,7 +42,6 @@ public class NewsListPresenter extends BasePresenter<INewsListView> {
         mRetrofitServices.getService(NewsService.class).list(page, limit).enqueue(new NetworkCallback<List<News>>() {
             @Override
             public void onResponseSuccessful(List<News> news) {
-                news = getTestNews();
                 if (news.size() > 0) {
                     getView().onGetNewsSuccess(news);
 
@@ -108,35 +107,5 @@ public class NewsListPresenter extends BasePresenter<INewsListView> {
                 getView().onGetNewsError(ErrorCode.INERNET_CONNECTION_ERROR);
             }
         });
-    }
-
-    private List<News> getTestNews() {
-        News news1 = new News();
-        news1.setId(-1);
-        news1.setTitle("aaaa¿Famosos y sólo amigos?");
-        news1.setPicture("http://bucket1.glanacion.com/anexos/fotos/70/dia-del-amigo-2236070w620.jpg");
-        news1.setText("Ser súper estrellas e íntimos amigos tiene sus desventajas, al menos para George. Su esposa, Amal, es muy celosa de Julia e irrumpió varias veces en las grabaciones de su última peli juntos, aunque nunca pescó nada raro.");
-        news1.setCreatedAt(new DateTime(2018, 8, 14, 10, 59));
-        news1.setLikes(Arrays.asList(1, 3));
-
-        News news2 = new News();
-        news2.setId(-2);
-        news2.setTitle("bbbbbHipnosis: la nueva vedette de las neurociencias");
-        news2.setPicture("http://bucket1.glanacion.com/anexos/fotos/50/2082050.jpg");
-        news2.setText("Hace un año, Marisa Bello, bibliotecóloga de La Plata, separada, 51 años, condujo un auto ultralujoso. Luego, durmió profundamente. Más tarde, se rió a carcajadas y olió un perfume indescriptible. Todo eso lo vivió desde una silla, apostada en el escenario de un pabellón de Tecnópolis. Para ella, sucedió durante una hora. \\\"En realidad, estuvo entre dieciséis y dieciocho minutos, que es el tiempo máximo que utilizamos durante nuestros espectáculos para hipnotizar a la gente -explica Gonzalo Blanc, un abogado de 41 años-. Pero la percepción del tiempo en ese estado es otra, y eso la llevó a sentir la experiencia mucho más larga\\\". Durante dieciséis presentaciones en Tecnópolis, Gonzalo, junto con el médico Daniel West, de 30 años, practicaron hipnosis colectiva sobre el público. Los dos viven en Montevideo y se dedican desde hace más de diez años a investigar las neurociencias. Dan seminarios, conferencias y talleres empresariales para mejorar el rendimiento a través de la hipnosis -tuvieron clientes como YPF, Telefónica, L'Oréal y Santillana-; practican hipnosis clínica para atenuar el dolor y curar patologías, y sus conferencias en TEDx Durazno y en TEDx Río de la Plata, llamadas \\\"¿Se puede entrenar a la mente para ser exitosos?\\\", tienen más de 150.000 reproducciones.");
-        news2.setCreatedAt(new DateTime(2018, 8, 13, 23, 12));
-
-        return new ArrayList<News>() {{
-            add(news1);
-            add(news2);
-            add(news1);
-            add(news2);
-            add(news1);
-            add(news2);
-            add(news1);
-            add(news2);
-            add(news1);
-            add(news2);
-        }};
     }
 }
