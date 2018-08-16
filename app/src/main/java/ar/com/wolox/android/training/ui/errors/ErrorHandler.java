04db50b0f78068289c24fd1c730101589ca44670
Dylan Muszel <dylan.muszel@wolox.com.ar>
2018-08-16 16:47:07 -0300

    Fix error handler

--- app/src/main/java/ar/com/wolox/android/training/ui/errors/ErrorHandler.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/errors/ErrorHandler.java
@@ -6,28 +6,31 @@ import ar.com.wolox.android.R;
 
 public class ErrorHandler {
     public static String getErrorMessage(ErrorCode errorCode) {
-        int code = 0;
+        int code;
         switch (errorCode) {
             case EMPTY_FIELDS:
                 code = R.string.error_empty_fields;
+                break;
 
             case INVALID_EMAIL:
                 code = R.string.error_invalid_email;
-                return "El email ingresado es incorrecto";
+                break;
 
             case INVALID_CREDENTIALS:
                 code = R.string.error_invalid_credentials;
-                return "Usuario o clave incorrectos";
+                break;
 
             case INERNET_CONNECTION_ERROR:
                 code = R.string.error_internet_connection_error;
-                return "Error en la conexion a internet";
+                break;
 
             case NO_NEWS:
                 code = R.string.error_no_news;
+                break;
 
             default:
-                code = R.string.error_unexpected;
+                code= R.string.error_unexpected;
+                break;
         }
 
         return Resources.getSystem().getString(code);
